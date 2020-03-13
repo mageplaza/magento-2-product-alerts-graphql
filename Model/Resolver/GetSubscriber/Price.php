5e04339f5d70de21f429516c294a348f787e5a08
eric <thainc@mageplaza.com>
2020-03-13 10:50:59 +0700

    fix comment

--- Model/Resolver/GetSubscriber/Price.php
+++ Model/Resolver/GetSubscriber/Price.php
@@ -14,7 +14,7 @@
  * version in the future.
  *
  * @category    Mageplaza
- * @package     Mageplaza_FaqsGraphQl
+ * @package     Mageplaza_ProductAlertsGraphQl
  * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
  * @license     https://www.mageplaza.com/LICENSE.txt
  */
